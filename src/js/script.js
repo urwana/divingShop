4dbf185a2ae055da0755b82e82f91c11fd2c047d
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-04-11 16:06:36 +0900

    hamburger

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,12 @@
-
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
-
+jQuery(function($) {
+  $(".js-hamburger").on("click", function(e) {
+    e.preventDefault();
+    if ($(".js-hamburger").hasClass("is-open")) {
+      $(this).removeClass("is-open");
+      $(".js-drawer-menu").removeClass("is-open");
+    } else {
+      $(this).addClass("is-open");
+      $(".js-drawer-menu").addClass("is-open");
+    }
+  });
 });
