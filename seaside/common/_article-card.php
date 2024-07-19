4206a5e3a40eb8a8c1a3cb80c2e50d8efc548038
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-07-20 00:08:21 +0900

    blog bug 修正

--- seaside/common/_article-card.php
+++ seaside/common/_article-card.php
@@ -1,14 +1,16 @@
-<?php $articleCard = $args["articleCard"]; ?>
+<?php $articleCard = isset($args["articleCard"]) ? $args["articleCard"] : []; ?>
 <div class="article-card">
-  <a class="article-card__inner" href="<?php echo $articleCard["link"] ?>">
+  <a class="article-card__inner" href="<?php echo isset($articleCard["link"]) ? $articleCard["link"] : '#'; ?>">
     <figure class="article-card__image">
       <img src="<?php echo get_template_directory_uri();
-                echo $articleCard["imgPath"] ?>" alt="<?php echo $articleCard["alt"] ?>" width="121" height="90" />
+                echo isset($articleCard["imgPath"]) ? $articleCard["imgPath"] : ''; ?>"
+        alt="<?php echo isset($articleCard["alt"]) ? $articleCard["alt"] : ''; ?>" width="121" height="90" />
     </figure>
     <div class="article-card__body">
       <time class="article-card__date"
-        datetime="<?php echo $articleCard["datetime"] ?>"><?php echo $articleCard["date"] ?></time>
-      <div class="article-card__title"><?php echo $articleCard["title"] ?></div>
+        datetime="<?php echo isset($articleCard["datetime"]) ? $articleCard["datetime"] : ''; ?>"><?php echo isset($articleCard["date"]) ? $articleCard["date"] : ''; ?>
+      </time>
+      <div class="article-card__title"><?php echo isset($articleCard["title"]) ? $articleCard["title"] : ''; ?></div>
     </div>
   </a>
 </div>
--- seaside/common/_sidebar-popular-articles.php
+++ seaside/common/_sidebar-popular-articles.php
@@ -3,22 +3,31 @@
 
   <?php $articleCardDataAll = [
     [
-      "link " => "/blog-detail.html", "imgPath" =>
-      "/assets/images/blog/popular-article1.jpg", "alt " => "ライセンス取得の画像",
-      "date" => "2023/11/17", "datetime" => "2023-11-17", "title " => "ライセンス取得", "text" =>
-      "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキスト",
+      "link " => "/blog-detail.html",
+      "imgPath" => "/assets/images/blog/popular-article1.jpg",
+      "alt " => "ライセンス取得の画像",
+      "date" => "2023/11/17",
+      "datetime" => "2023-11-17",
+      "title " => "ライセンス取得",
+      "text" => "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキスト",
     ],
     [
-      "link" => "/blog-detail.html", "imgPath" =>
-      "/assets/images/blog/popular-article2.jpg", "alt" => "ウミガメと泳ぐ", "date" =>
-      "2023/11/17", "datetime" => "2023-11-17", "title" => "ウミガメと泳ぐ", "text" =>
-      "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキスト",
+      "link" => "/blog-detail.html",
+      "imgPath" => "/assets/images/blog/popular-article2.jpg",
+      "alt" => "ウミガメと泳ぐ",
+      "date" => "2023/11/17",
+      "datetime" => "2023-11-17",
+      "title" => "ウミガメと泳ぐ",
+      "text" => "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキスト",
     ],
     [
-      "link " => "/blog-detail.html", "imgPath" =>
-      "/assets/images/blog/popular-article3.jpg", "alt" => "カクレクマノミ", "date" =>
-      "2023/11/17", "datetime" => "2023-11-17", "title" => "カクレクマノミ", "text" =>
-      "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキスト",
+      "link " => "/blog-detail.html",
+      "imgPath" => "/assets/images/blog/popular-article3.jpg",
+      "alt" => "カクレクマノミ",
+      "date" => "2023/11/17",
+      "datetime" => "2023-11-17",
+      "title" => "カクレクマノミ",
+      "text" => "ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキスト",
     ],
   ]; ?>
   <div class="sidebar-popular-articles__cards">
