75a888eec43237eed55f0cc6b6c4199505d65887
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-07-25 18:50:02 +0900

    price 修正

--- seaside/common/_price-list.php
+++ seaside/common/_price-list.php
@@ -1,27 +1,33 @@
-<?php $price_lists_item = $args["price_lists_item"]; ?>
+<?php $price_list_group = $args["price_list_group"]; ?>
 
-<div class="price-lists__item">
-  <div class="price-list">
-    <div id="licence" class="price-list__title">
-      <div class="price-list__title-inner">
-        <p><?php $price_lists_item["plice_list_title"] ?></p>
-        <figure class="price-list__title-icon">
-          <img src="http://seaside.local/wp-content/themes/seaside/assets/images/price/whale.svg" alt="鯨のアイコン"
-            width="64" height="64" />
-        </figure>
-      </div>
+<?php
+// echo '<pre>';
+// var_dump($price_lists_item);
+// echo '</pre>';
+?>
+
+<div class="price-list">
+  <div id="licence" class="price-list__title">
+    <div class="price-list__title-inner">
+      <p><?php echo $price_list_group["price_list_title"] ?></p>
+      <figure class="price-list__title-icon">
+        <img src="http://seaside.local/wp-content/themes/seaside/assets/images/price/whale.svg" alt="鯨のアイコン" width="64"
+          height="64" />
+      </figure>
     </div>
-    <dl class="price-list__body">
-      <?php
-        $price_list_body=$price_lists_item["price_list_body"];
-        foreach ($pricelist_body as $price_list) { ?>
-      <div class="price-list__item">
-        <dt class="price-list__name">
-          <?php $price_list["price_title"] ?>
-        </dt>
-        <dd class="price-list__price"> <?php $price_list["price"] ?></dd>
-      </div>
-      <?php } ?>
-    </dl>
   </div>
+
+
+  <dl class="price-list__body">
+    <?php
+    $price_list_body = $price_list_group["price_list_body"];
+    foreach ($price_list_body as $price_list) { ?>
+    <div class="price-list__item">
+      <dt class="price-list__name">
+        <?php echo $price_list["price_title"] ?>
+      </dt>
+      <dd class="price-list__price"> <?php echo $price_list["price"] ?></dd>
+    </div>
+    <?php } ?>
+  </dl>
 </div>
--- seaside/common/_price-lists.php
+++ seaside/common/_price-lists.php
@@ -1,44 +1,16 @@
 <?php
 $price_lists = CFS()->get('price_lists');
 ?>
-<?php if (!empty($price_lists) && isset($price_lists[0]['price_lists_item'])) { ?>
-<div class="price-lists">
-  <?php foreach ($price_lists[0]['price_lists_item'] as $price_lists_item) { ?>
 
-  <?php
-//デバッグ用に配列の内容を表示
-// echo '<pre>';
-// var_dump($price_lists_item["price_list_body"]);
-//echo '</pre>';
+<?php
+$price_lists_item = $price_lists[0]["price_lists_item"];
 ?>
-
-
+<?php if (!empty($price_lists) && isset($price_lists_item)) { ?>
+<div class="price-lists">
+  <?php foreach ($price_lists_item as $price_list_group) { ?>
   <div class="price-lists__item">
-    <div class="price-list">
-      <div id="licence" class="price-list__title">
-        <div class="price-list__title-inner">
-          <p><?php $price_lists_item["plice_list_title"] ?></p>
-          <figure class="price-list__title-icon">
-            <img src="http://seaside.local/wp-content/themes/seaside/assets/images/price/whale.svg" alt="鯨のアイコン"
-              width="64" height="64" />
-          </figure>
-        </div>
-      </div>
-      <dl class="price-list__body">
-        <?php 
-        $price_list_body=$price_lists_item["price_list_body"];
-        foreach ($price_list_body as $price_list) { ?>
-        <div class="price-list__item">
-          <dt class="price-list__name">
-            <?php echo $price_list["price_title"] ?>
-          </dt>
-          <dd class="price-list__price"> <?php echo $price_list["price"] ?></dd>
-        </div>
-        <?php } ?>
-      </dl>
-    </div>
+    <?php get_template_part("/common/_price-list", null,  ["price_list_group" => $price_list_group]); ?>
   </div>
-  <?php get_template_part("/common/_price-list", null,  ["price_lists_item" => $price_lists_item]); ?>
+  <?php } ?>
 </div>
-<?php } ?>
 <?php }; ?>
