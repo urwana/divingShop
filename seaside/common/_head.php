c5442ed4781ed47c24b5a7be72306bbd69da4451
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-07-28 01:16:38 +0900

    noindex

--- seaside/common/_head.php
+++ seaside/common/_head.php
@@ -2,7 +2,6 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0" />
   <meta name="format-detection" content="telephone=no" />
-  <meta name="robots" content="noindex, nofollow" />
   <!-- ファビコン -->
   <link rel="icon" href="./favicon.ico" type="image/x-icon" />
   <?php wp_head(); ?>
