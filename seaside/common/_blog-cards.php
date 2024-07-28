e5a41e66d91b3dd3ccb81232df1c7170246405fa
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-07-28 17:49:01 +0900

    campaign バグ修正4

--- seaside/common/_blog-cards.php
+++ seaside/common/_blog-cards.php
@@ -1,35 +1,26 @@
-<?php $top = $args["top"]; ?>
+<?php $top = isset($args["top"]) ? $args["top"] : false; ?>
 
-<div class="<?php echo $top ? 'blog-cards' : 'blog-cards blog-cards--2col' ?>">
+<div class="<?php echo $top ? 'blog-cards' : 'blog-cards blog-cards--2col'; ?>">
   <?php
   if (is_front_page()) {
-    $front_page_args = array(
+    $query_args = array(
       'post_type' => 'post',
-      'posts_per_page' => 10
+      'posts_per_page' => $top ? 3 : 10
     );
-    $front_page_query = new WP_Query($front_page_args);
-    if ($front_page_query->have_posts()) :
-      while ($front_page_query->have_posts()) :
-        $front_page_query->the_post(); ?>
-  <div class="blog-cards__item">
-    <?php get_template_part("/common/_blog-card") ?>
-  </div>
-  <?php endwhile;
-      wp_reset_postdata();
-    else : ?>
-  <p>ブログ記事の投稿はありません。</p>
-  <?php endif;
-  } elseif (is_home()) {
-    if (have_posts()) :
-      while (have_posts()) :
-        the_post(); ?>
-  <div class="blog-cards__item">
-    <?php get_template_part("/common/_blog-card") ?>
-  </div>
-  <?php endwhile;
-    else : ?>
-  <p>ブログ記事の投稿はありません。</p>
-  <?php endif;
+    $the_query = new WP_Query($query_args);
+  } else {
+    $the_query = $wp_query; // デフォルトのクエリを使用
   }
-  ?>
+
+  if ($the_query->have_posts()) {
+    while ($the_query->have_posts()) {
+      $the_query->the_post(); ?>
+  <div class="blog-cards__item">
+    <?php get_template_part("/common/_blog-card"); ?>
+  </div>
+  <?php }
+    wp_reset_postdata();
+  } else { ?>
+  <p>ブログ記事の投稿はありません。</p>
+  <?php } ?>
 </div>
