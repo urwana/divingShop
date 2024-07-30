01001db2516b8f64290a67d553def79616242ef8
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-07-30 22:49:27 +0900

    campaign cards loop 修正

--- seaside/common/_campaign-cards.php
+++ seaside/common/_campaign-cards.php
@@ -4,33 +4,39 @@
 <div class="<?php echo $sideBar ? 'campaign-cards--sidebar' : 'campaign-cards'; ?>">
   <?php
   $taxonomy = 'campaign_taxonomy';
-  $term_slug = get_query_var('term') ? get_query_var('term') : null;
-  $tax_query = [];
-  if ($term_slug) {
-    $tax_query = [
+  $term_slug = get_query_var('term');
+  $campaign_args = [
+    'post_type' => 'campaign',
+    'posts_per_page' => $sideBar ? 2 : 4,
+    'tax_query' => $term_slug ? [
       [
         'taxonomy' => $taxonomy,
         'field'    => 'slug',
         'terms'    => $term_slug,
       ],
-    ];
-  };
-  $campaign_args = [
-    'post_type' => 'campaign',
-    'posts_per_page' => $sideBar ? 2 : 4,
-    'tax_query' => $tax_query,
-    'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1,
+    ] : [],
+    'paged' => get_query_var('paged', 1),
   ];
-  ?>
-  <?php
-  $the_query = new WP_Query($campaign_args);
-  if ($the_query->have_posts()) :
-    while ($the_query->have_posts()) :
-      $the_query->the_post(); ?>
+  if ($page) {
+    if (have_posts()) :
+      while (have_posts()) :
+        the_post(); ?>
   <?php get_template_part('/common/_campaign-card-page', null, ['page' => $page, 'sideBar' => $sideBar]); ?>
-  <?php endwhile; ?>
-  <?php else : ?>
+  <?php endwhile;
+    else : ?>
   <p>投稿が見つかりませんでした。</p>
-  <?php endif; ?>
-  <?php wp_reset_postdata(); ?>
+  <?php endif;
+  } else {
+    $the_query = new WP_Query($campaign_args);
+    if ($the_query->have_posts()) :
+      while ($the_query->have_posts()) :
+        $the_query->the_post(); ?>
+  <?php get_template_part('/common/_campaign-card-page', null, ['page' => $page, 'sideBar' => $sideBar]); ?>
+  <?php endwhile;
+    else : ?>
+  <p>投稿が見つかりませんでした。</p>
+  <?php endif;
+    wp_reset_postdata();
+  }
+  ?>
 </div>
