7c2e9908214bad90efebb7826d5ae0533e26aff9
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-07-30 14:31:38 +0900

    price id デグレ修正

--- seaside/common/_price-lists.php
+++ seaside/common/_price-lists.php
@@ -8,7 +8,7 @@ $price_lists_item = $price_lists[0]["price_lists_item"];
 <?php if (!empty($price_lists) && isset($price_lists_item)) : ?>
 <div class="price-lists">
   <?php foreach ($price_lists_item as $price_list_group) : ?>
-  <div class="price-lists__item">
+  <div class="price-lists__item" id="<?php echo $price_list_group["price_list_id"]; ?>">
     <?php get_template_part("/common/_price-list", null,  ["price_list_group" => $price_list_group]); ?>
   </div>
   <?php endforeach; ?>
