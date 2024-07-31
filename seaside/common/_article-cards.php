e2310fb4adb94f9ba93cb65a5f2b8ada77fa2a4a
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-07-31 15:06:56 +0900

    人気投稿5

--- seaside/common/_article-cards.php
+++ seaside/common/_article-cards.php
@@ -1,16 +1,15 @@
 <div class="article-cards">
   <?php
   // 投稿タイプとステータスを確認するクエリ
-  $test_query = new WP_Query(array(
-    'post_type' => 'blog',
+  $test_query = new WP_Query(array('post_type' => 'post',
     'post_status' => array('publish', 'acf-disabled'),
     'posts_per_page' => -1
   ));
 
   // デバッグ情報を出力する
-  echo '<pre>';
-  var_dump($test_query);
-  echo '</pre>';
+  // echo '<pre>';
+  // var_dump($test_query);
+  // echo '</pre>';
   $popular_posts = get_popular_posts();
   if ($popular_posts->have_posts()) :
   
