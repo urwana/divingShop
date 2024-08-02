4b28fb13fc07ecdd6ce7285fad3c20ce4a5dbf09
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-08-02 18:33:19 +0900

    price list 修正

--- seaside/common/_price-lists.php
+++ seaside/common/_price-lists.php
@@ -6,11 +6,32 @@ $price_lists = CFS()->get('price_lists');
 $price_lists_item = $price_lists[0]["price_lists_item"];
 ?>
 <?php if (!empty($price_lists) && isset($price_lists_item)) : ?>
-<div class="price-lists">
-  <?php foreach ($price_lists_item as $price_list_group) : ?>
-  <div class="price-lists__item" id="<?php echo $price_list_group["price_list_id"]; ?>">
-    <?php get_template_part("/common/_price-list", null,  ["price_list_group" => $price_list_group]); ?>
+  <div class="price-lists">
+    <?php foreach ($price_lists_item as $price_list_group) : ?>
+      <div class="price-lists__item" id="<?php echo $price_list_group["price_list_id"]; ?>">
+        <div class="price-list">
+          <div id="licence" class="price-list__title">
+            <div class="price-list__title-inner">
+              <p><?php echo $price_list_group["price_list_title"]; ?></p>
+              <figure class="price-list__title-icon">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/price/whale.svg" alt="鯨のアイコン" width="64" height="64" />
+              </figure>
+            </div>
+          </div>
+          <dl class="price-list__body">
+            <?php
+            $price_list_body = $price_list_group["price_list_body"];
+            foreach ($price_list_body as $price_list) : ?>
+              <div class="price-list__item">
+                <dt class="price-list__name">
+                  <?php echo $price_list["price_title"] ?>
+                </dt>
+                <dd class="price-list__price"> <?php echo $price_list["price"]; ?></dd>
+              </div>
+            <?php endforeach; ?>
+          </dl>
+        </div>
+      </div>
+    <?php endforeach; ?>
   </div>
-  <?php endforeach; ?>
-</div>
 <?php endif; ?>
