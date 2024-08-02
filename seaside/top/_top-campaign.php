b18cdd6b639e262320dff6b98bdfceb687137665
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-08-02 23:28:10 +0900

    swiper 統合

--- seaside/top/_top-campaign.php
+++ seaside/top/_top-campaign.php
@@ -8,7 +8,34 @@
         </div>
       </div>
       <div class="top-campaign__swiper">
-        <?php get_template_part("/common/_swiper-campaign"); ?>
+        <?php
+        $args = array(
+          "post_type" => "campaign",
+          'posts_per_page' => 4,
+        );
+        $the_query = new WP_Query($args);
+        ?>
+        <div class="js-swiper-campaign swiper-campaign">
+          <div class="swiper-wrapper swiper-campaign__wrapper">
+            <?php if (have_posts()) {
+              while ($the_query->have_posts()) {
+                $the_query->the_post();
+            ?>
+                <div class="swiper-slide swiper-campaign__swiper-slide">
+                  <div class="top-campaign__card">
+                    <?php get_template_part('/common/_campaign-card', null, ["page" => false]) ?>
+                  </div>
+                </div>
+            <?php }
+            }; ?>
+          </div>
+          <div class="swiper-campaign__button-prev u-desktop">
+            <div class="button-scroll--left"></div>
+          </div>
+          <div class="swiper-campaign__button-next u-desktop">
+            <div class="button-scroll--right"></div>
+          </div>
+        </div>
       </div>
       <div class="top-campaign__button">
         <a href="/seaside/campaign" class="button"><span class="button__text">View more</span></a>
