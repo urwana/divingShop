5ad2531ef0371eebf4fdecf1129ddb9099e27910
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-08-03 02:24:12 +0900

    date デグレ修正

--- seaside/date.php
+++ seaside/date.php
@@ -18,7 +18,41 @@
       <div class="blog__inner inner">
         <div class="blog__body">
           <div class="blog__cards">
-            <?php get_template_part("/common/_blog-cards", null, ["page" => true]); ?>
+            <div class="blog-cards blog-cards--2col">
+              <?php
+              if (have_posts()) :
+                while (have_posts()) :
+                  the_post(); ?>
+                  <div class="blog-cards__item">
+                    <div class="blog-card">
+                      <a href="<?php the_permalink() ?>">
+                        <div class="blog-card__inner">
+                          <?php
+                          if (has_post_thumbnail()) :
+                          ?>
+                            <figure class="campaign-card__image">
+                              <img src="<?php the_post_thumbnail_url("full"); ?>" alt="<?php the_title(); ?>" />
+                            </figure>
+                          <?php
+                          endif;
+                          ?>
+                          <div class="blog-card__content">
+                            <time class="blog-card__date" datetime="<?php the_time("c"); ?>"><?php the_time("Y/m/d"); ?></time>
+                            <div class="blog-card__title"><?php the_title(); ?></div>
+                            <div class="blog-card__text">
+                              <?php get_custom_excerpt(92); ?>
+                            </div>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                <?php endwhile;
+              else : ?>
+                <p>ブログ記事の投稿はありません。</p>
+              <?php endif;
+              ?>
+            </div>
             <div class="blog__pager">
               <?php get_template_part("/common/_pager"); ?>
             </div>
