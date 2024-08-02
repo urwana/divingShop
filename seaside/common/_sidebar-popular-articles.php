a2433c322ba7b1b4c2985ba36326718a68ac0568
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-08-02 19:20:57 +0900

    sidebar2

--- seaside/common/_sidebar-popular-articles.php
+++
@@ -1,7 +0,0 @@
-<div class="sidebar-popular-articles">
-  <?php get_template_part("/common/_sidebar-title", null, ["title" => "人気記事", "className" => "sidebar-popular-articles__title"]) ?>
-  <!--TODO: ランキング実装確認 -->
-  <div class="sidebar-popular-articles__cards">
-    <?php get_template_part("/common/_article-cards"); ?>
-  </div>
-</div>
