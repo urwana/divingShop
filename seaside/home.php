518a60bb365948c7d4a34fb55cf4d90c287acf9e
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-08-02 20:44:53 +0900

    home

--- seaside/home.php
+++ seaside/home.php
@@ -12,13 +12,24 @@
       <div class="blog__inner inner">
         <div class="blog__body">
           <div class="blog__cards">
-            <?php get_template_part("/common/_blog-cards", null, ["page" => true]); ?>
+            <div class="blog-cards--2col">
+              <?php
+              if (have_posts()) :
+                while (have_posts()) :
+                  the_post(); ?>
+                  <div class="blog-cards__item">
+                    <?php get_template_part("/common/_blog-card"); ?>
+                  </div>
+                <?php endwhile;
+              else : ?>
+                <p>ブログ記事の投稿はありません。</p>
+              <?php endif;
+              ?>
+            </div>
             <div class="blog__pager">
               <?php get_template_part("/common/_pager"); ?>
             </div>
           </div>
-
-
           <?php get_sidebar(); ?>
         </div>
       </div>
