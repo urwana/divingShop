c350c912ec83d0ca46f9a7294ec1f997a8c950cd
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-08-05 21:17:15 +0900

    tab card 記載修正

--- seaside/common/_tab-card.php
+++ seaside/common/_tab-card.php
@@ -5,24 +5,23 @@
   <a href="<?php echo esc_url(get_post_type_archive_link($post_type)); ?>"
     class="tab-card__item <?php echo empty(get_query_var($taxonomy)) ? 'current' : ''; ?>">ALL</a>
   <?php
-  if (taxonomy_exists($taxonomy)) {
+  if (taxonomy_exists($taxonomy)) :
     $terms = get_terms([
       'taxonomy' => $taxonomy,
       'hide_empty' => false,
     ]);
-
-    if (is_wp_error($terms)) {
-      echo '<div>タブの取得に失敗しました: ' . $terms->get_error_message() . '</div>';
-    } elseif (empty($terms)) {
-      echo '<div>タブがありません。</div>';
-    } else {
-      foreach ($terms as $term) {
-        $term_link = get_term_link($term, $taxonomy);
-        echo '<a href="' . esc_url($term_link) . '" class="tab-card__item ' . (is_tax($taxonomy, $term->slug) ? 'current' : '') . '">' . esc_html($term->name) . '</a>';
-      }
-    }
-  } else {
-    echo '<div>指定されたタクソノミーが存在しません。</div>';
-  }
-  ?>
+    if (is_wp_error($terms)) : ?>
+  <div>タブの取得に失敗しました:<?php $terms->get_error_message() ?></div>
+  <?php elseif (empty($terms)) : ?>
+  <div>タブがありません。</div>
+  <?php else :
+      foreach ($terms as $term) :
+        $term_link = get_term_link($term, $taxonomy); ?>
+  <a href="<?php esc_url($term_link) ?>"
+    class="tab-card__item <?php echo is_tax($taxonomy, $term->slug) ? 'current' : ''; ?>"><?php echo esc_html($term->name) ?></a>
+  <?php endforeach;
+    endif; ?>
+  <?php else : ?>
+  <div>指定されたタクソノミーが存在しません。</div>
+  <?php endif ?>
 </div>
