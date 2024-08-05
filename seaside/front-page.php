be9e55a294127f6eb9b84607879d3041490e614d
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-08-05 17:54:01 +0900

    top デグレ修正

--- seaside/front-page.php
+++ seaside/front-page.php
@@ -142,34 +142,36 @@
   <section class="l-top-about">
     <?php get_template_part("/common/_about", null, ["top" => true]); ?>
   </section>
-  <div class="top-information">
-    <div class="top-information__inner inner">
-      <div class="top-information__title">
-        <div class="section-title">
-          <p class="section-title__word">Information</p>
-          <h2 class="section-title__japan">ダイビング情報</h2>
-        </div>
-      </div>
-      <div class="top-information__contents">
-        <figure class="js-colorAnimation top-information__image">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/information.jpg" alt="海の中 珊瑚と魚たち"
-            width="540" height="356" />
-        </figure>
-        <div class="top-information__info">
-          <div class="top-information__info-title">
-            <div class="lined-title lined-title--medium">ライセンス講習</div>
+  <section class="l-top-information">
+    <div class="top-information">
+      <div class="top-information__inner inner">
+        <div class="top-information__title">
+          <div class="section-title">
+            <p class="section-title__word">Information</p>
+            <h2 class="section-title__japan">ダイビング情報</h2>
           </div>
-          <p class="top-information__text">
-            当店はダイビングライセンス（Cカード）世界最大の教育機関PADIの「正規店」として店舗登録されています。<br />
-            正規登録店として、安心安全に初めての方でも安心安全にライセンス取得をサポート致します。
-          </p>
-          <div class="top-information__button">
-            <a href="/seaside/information" class="button"><span class="button__text">View more</span></a>
+        </div>
+        <div class="top-information__contents">
+          <figure class="js-colorAnimation top-information__image">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/information.jpg" alt="海の中 珊瑚と魚たち"
+              width="540" height="356" />
+          </figure>
+          <div class="top-information__info">
+            <div class="top-information__info-title">
+              <div class="lined-title lined-title--medium">ライセンス講習</div>
+            </div>
+            <p class="top-information__text">
+              当店はダイビングライセンス（Cカード）世界最大の教育機関PADIの「正規店」として店舗登録されています。<br />
+              正規登録店として、安心安全に初めての方でも安心安全にライセンス取得をサポート致します。
+            </p>
+            <div class="top-information__button">
+              <a href="/seaside/information" class="button"><span class="button__text">View more</span></a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
   <section class="l-top-blog">
     <div class="top-blog">
       <div class="top-blog__inner inner">
