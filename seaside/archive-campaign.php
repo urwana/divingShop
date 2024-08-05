209688f7660c6d277349ff505e8219567a2bc735
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-08-05 22:30:18 +0900

    top price loop

--- seaside/archive-campaign.php
+++ seaside/archive-campaign.php
@@ -16,38 +16,24 @@
         <div class="campaign__cards">
           <div class="campaign-cards">
             <?php
-            $taxonomy = 'campaign_taxonomy';
-            $term_slug = get_query_var('term');
-            $campaign_args = [
-              'post_type' => 'campaign',
-              'posts_per_page' => 4,
-              'tax_query' => $term_slug ? [
-                [
-                  'taxonomy' => $taxonomy,
-                  'field'    => 'slug',
-                  'terms'    => $term_slug,
-                ],
-              ] : [],
-              'paged' => get_query_var('paged', 1),
-            ];
             if (have_posts()) :
               while (have_posts()) :
                 the_post(); ?>
-                <a class="campaign-card campaign-card--page" href="<?php echo esc_url(home_url("/campaign/")); ?>">
-                  <?php
+            <a class="campaign-card campaign-card--page" href="<?php echo esc_url(home_url("/campaign/")); ?>">
+              <?php
                   if (has_post_thumbnail()) :
                   ?>
-                    <figure class="campaign-card__image">
-                      <img src="<?php the_post_thumbnail_url("full"); ?>" alt="<?php the_title(); ?>" />
-                    </figure>
-                  <?php
+              <figure class="campaign-card__image">
+                <img src="<?php the_post_thumbnail_url("full"); ?>" alt="<?php the_title(); ?>" />
+              </figure>
+              <?php
                   endif;
                   ?>
-                  <div class="campaign-card__body">
-                    <div class="campaign-card__top">
-                      <div class="campaign-card__label label-container">
-                        <span class="label">
-                          <?php
+              <div class="campaign-card__body">
+                <div class="campaign-card__top">
+                  <div class="campaign-card__label label-container">
+                    <span class="label">
+                      <?php
                           $post_id = get_the_ID();
                           $campaign_terms = get_the_terms($post_id, 'campaign_taxonomy');
                           if ($campaign_terms && !is_wp_error($campaign_terms)) :
@@ -55,26 +41,26 @@
                               echo esc_html($term->name) . ' ';
                             endforeach;
                           endif; ?>
-                        </span>
-                      </div>
-                      <div class="campaign-card__title"><?php the_title(); ?></div>
-                    </div>
-                    <div class="campaign-card__bottom">
-                      <p class="campaign-card__explain">
-                        <?php the_field("main_text"); ?>
-                      </p>
-                      <div class="campaign-card__price-container">
-                        <div class="price-container">
-                          <span class="price-container__cancelled-price">¥<?php the_field("cancelled_price") ?></span>
-                          <span class="price-container__price">¥<?php the_field("price") ?></span>
-                        </div>
-                      </div>
+                    </span>
+                  </div>
+                  <div class="campaign-card__title"><?php the_title(); ?></div>
+                </div>
+                <div class="campaign-card__bottom">
+                  <p class="campaign-card__explain">
+                    <?php the_field("main_text"); ?>
+                  </p>
+                  <div class="campaign-card__price-container">
+                    <div class="price-container">
+                      <span class="price-container__cancelled-price">¥<?php the_field("cancelled_price") ?></span>
+                      <span class="price-container__price">¥<?php the_field("price") ?></span>
                     </div>
                   </div>
-                </a>
-              <?php endwhile; ?>
+                </div>
+              </div>
+            </a>
+            <?php endwhile; ?>
             <?php else : ?>
-              <p>投稿が見つかりませんでした。</p>
+            <p>投稿が見つかりませんでした。</p>
             <?php endif;
             wp_reset_postdata();
             ?>
--- seaside/front-page.php
+++ seaside/front-page.php
@@ -247,24 +247,9 @@
         <div class="top-voice__cards">
           <div class="voice-cards">
             <?php
-            $taxonomy = 'voice_taxonomy';
-            $term_slug = get_query_var('term');
-
-            $tax_query = [];
-            if ($term_slug) {
-              $tax_query = [
-                [
-                  'taxonomy' => $taxonomy,
-                  'field'    => 'slug',
-                  'terms'    => $term_slug,
-                ],
-              ];
-            }
             $voice_args = [
               'post_type' => 'voice',
               'posts_per_page' => 2,
-              'tax_query' => $tax_query,
-              'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1,
             ];
             $the_voice_query = new WP_Query($voice_args);
             if ($the_voice_query->have_posts()) :
