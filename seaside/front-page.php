55c6d93aff6251b02e50ac6501e893918974965c
Tomoo Suzuki <solosoprani2002@gmail.com>
2024-08-14 09:34:51 +0900

    top swiper fall back6

--- seaside/front-page.php
+++ seaside/front-page.php
@@ -28,32 +28,6 @@
           </p>
         </div>
       </div>
-      <!-- <div class="swiper-wrapper key-visual__swiper-wrapper">
-        <figure class="swiper-slide key-visual__swiper-slide">
-          <picture>
-            <source media="(max-width: 767px)" srcset="<?php the_field("top_swiper_sp1") ?>" />
-            <img src="<?php the_field("top_swiper_1") ?>" alt="<?php the_field("top_swiper_alt1") ?>" />
-          </picture>
-        </figure>
-        <figure class="swiper-slide key-visual__swiper-slide">
-          <picture>
-            <source media="(max-width: 767px)" srcset="<?php the_field("top_swiper_sp2") ?>" />
-            <img src="<?php the_field("top_swiper_2") ?>" alt="<?php the_field("top_swiper_alt2") ?>" />
-          </picture>
-        </figure>
-        <figure class="swiper-slide key-visual__swiper-slide">
-          <picture>
-            <source media="(max-width: 767px)" srcset="<?php the_field("top_swiper_sp3") ?>" />
-            <img src="<?php the_field("top_swiper_3") ?>" alt="<?php the_field("top_swiper_alt3") ?>" />
-          </picture>
-        </figure>
-        <figure class="swiper-slide key-visual__swiper-slide">
-          <picture>
-            <source media="(max-width: 767px)" srcset="<?php the_field("top_swiper_sp4") ?>" />
-            <img src="<?php the_field("top_swiper_4") ?>" alt="<?php the_field("top_swiper_alt4") ?>" />
-          </picture>
-        </figure>
-      </div> -->
       <div class="swiper-wrapper key-visual__swiper-wrapper">
         <?php for ($i = 1; $i <= 5; $i++) : ?>
         <?php
