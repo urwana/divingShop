0e7c9b1792c7d24f386ba7e8bb345a13739e88fd
Tomoo Suzuki <solosoprani2002@gmail.com>
2025-06-29 23:06:43 +0900

    qa

--- seaside/page-price.php
+++ seaside/page-price.php
@@ -22,8 +22,8 @@
                   <div class="price-list__title-inner">
                     <p><?php echo $price_list_group["price_list_title"]; ?></p>
                     <figure class="price-list__title-icon">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/ceaser.webp" alt="鯨のアイコン"
-                        width="64" height="64" />
+                      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top/bg-ceaser.webp"
+                        alt="鯨のアイコン" width="64" height="64" />
                     </figure>
                   </div>
                 </div>
